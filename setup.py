4d4d894609f9fb1ab6e1c4ec35772eeae6d14aca
damck <damian.goworko@gmail.com>
2017-12-05 17:52:17 +0000

    added travis support

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='bio',
+      version='0.1',
+      description='Bioinformatics project',
+      author='Damian Goworko',
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest']
+     )
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def test_sequence():
+    assert 2 + 2 == 4
+
